0478bab4923a6b48c4553f1883597cbe42635ee6
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-01-19 22:53:24 +0100

    update password

--- app/components/dashboard/dashboard.controller.js
+++ app/components/dashboard/dashboard.controller.js
@@ -13,13 +13,51 @@
         $scope.currentUser.activePosition = _.find($scope.currentUser.positions, 'active');
         $scope.showEditProfileForm = showEditProfileForm;
         $scope.showEditPassForm = showEditPassForm;
+        $scope.cancelPassForm = cancelPassForm;
+        $scope.updatePassword = updatePassword;
+        $scope.checkPassForm = checkPassForm;
 
         function showEditProfileForm() {
             
         }
         
         function showEditPassForm() {
-            
+            $('#pass_form_modal').modal('show');
+        }
+
+        function cancelPassForm() {
+            $('#oldpass, #newpass, #confirm').val('');
+            $('#pass_form_modal').modal('hide');
+        }
+
+        function updatePassword(){
+            if(checkPassForm()){
+                UserService.updatePassword(
+                    $('#oldpass').val(),
+                    $('#newpass').val(),
+                    $('#confirm').val(),
+                    function (data) {
+                        if(data.code ==0){
+                            checkPassForm();
+                            MessageBoxService.showSuccess('Succès !', 'Mot de passe mis à jour avec succès !' );
+                        } else {
+                            MessageBoxService.handleServiceError(data);
+                        }
+                    }
+                )
+            }
+        }
+
+        function checkPassForm(){
+            $('#pass_form.field').removeClass("error");
+            var valid = true;
+            if ($('#newpass').val() != $('#confirm').val()) {
+                $('#newpass.field').addClass("error");
+                $('#confirm.field').addClass("error");
+                valid = false;
+                $('#pass_form').transition('shake');
+            }
+            return valid;
         }
     }
 })();
