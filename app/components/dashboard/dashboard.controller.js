8497f71d1536390c53e5def505a741cb47029f41
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-01-31 16:19:01 +0100

    controller without checkPassForm

--- app/components/dashboard/dashboard.controller.js
+++ app/components/dashboard/dashboard.controller.js
@@ -15,7 +15,6 @@
         $scope.showEditPassForm = showEditPassForm;
         $scope.cancelPassForm = cancelPassForm;
         $scope.updatePassword = updatePassword;
-        $scope.checkPassForm = checkPassForm;
         $scope.editState = false;
         $scope.olpass = "";
         $scope.nepass = "";
@@ -24,7 +23,7 @@
         function showEditProfileForm() {
             
         }
-        
+
         function showEditPassForm() {
             $scope.editState = true;
         }
@@ -37,33 +36,18 @@
         }
 
         function updatePassword(){
-            if(checkPassForm()){
-                UserService.updatePassword(
-                    $scope.olpass,
-                    $scope.nepass,
-                    $scope.confpass,
-                    function (data) {
-                        if(data.code ==0){
-                            checkPassForm();
-                            MessageBoxService.showSuccess('Succès !', 'Mot de passe mis à jour avec succès !' );
-                        } else {
-                            MessageBoxService.handleServiceError(data);
-                        }
+            UserService.updatePassword(
+                $scope.olpass,
+                $scope.nepass,
+                $scope.confpass,
+                function (data) {
+                    if(data.code ==0){
+                        MessageBoxService.showSuccess('Succès !', 'Mot de passe mis à jour avec succès !' );
+                    } else {
+                        MessageBoxService.handleServiceError(data);
                     }
-                )
-            }
-        }
-
-        function checkPassForm(){
-            element.find("pass_form").removeClass("error");
-            var valid = true;
-            if ($scope.nepass != $scope.confpass) {
-                element.find('newpass_field').addClass("error");
-                element.find('confirm_field').addClass("error");
-                valid = false;
-                element.find('pass_form').transition('shake');
-            }
-            return valid;
+                }
+            );
         }
     }
 })();
