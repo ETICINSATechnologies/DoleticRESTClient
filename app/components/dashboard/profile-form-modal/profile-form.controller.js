778c0d819e9427168db0f171ecbf559eb4736230
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-02-13 22:42:02 +0100

    update profile

--- app/components/dashboard/profile-form-modal/profile-form.controller.js
+++ app/components/dashboard/profile-form-modal/profile-form.controller.js
@@ -5,99 +5,20 @@
         .module('doleticApp')
         .controller('ProfileFormController', ProfileFormController);
 
-    ProfileFormController.$inject = ['$scope', '$state', 'SharedVariables', 'MessageBoxService', 'UserService'];
+    ProfileFormController.$inject = ['$scope', '$state', 'SharedVariables', 'MessageBoxService', 'UserService', 'CountryService', 'GenderService', 'YearService', 'DepartmentService'];
 
-    function ProfileFormController($scope, $state, SharedVariables, MessageBoxService, UserService) {
+    function ProfileFormController($scope, $state, SharedVariables, MessageBoxService, UserService, CountryService, GenderService, YearService, DepartmentService) {
 
         $scope.$state = $state;
         $scope.sharedVariables = SharedVariables;
         $scope.currentUser = UserService.getCurrentUser();
         $scope.currentUser.activePosition = _.find($scope.currentUser.positions, 'active');
-        $scope.cancelProfileForm = cancelProfileForm;
         $scope.updateProfile = updateProfile;
-        $scope.getAllCountries = getAllCountries;
-        $scope.getAllDepartments = getAllDepartments;
-        $scope.setGender = setGender;
-        $scope.setCountry = setCountry;
-        $scope.setYear = setYear;
-        $scope.setDepartment = setDepartment;
-        $scope.profile= {};
-        $scope.genders = [];
-        $scope.countries = [];
-        $scope.years = [];
-        $scope.departments = [];
-
-        function setGender(gender) {
-            $scope.profile.gender = gender;
-        }
-
-        function setCountry(country) {
-            $scope.profile.country = country;
-        }
-
-        function setYear(year) {
-            $scope.profile.year = year;
-        }
-
-        function setDepartment(dept){
-            $scope.profile.dept = dept;
-        }
-
-        function getAllCountries(){
-            CountryService.getAllCountries()
-                .success(
-                    function (data) {
-                        $scope.countries = data.countries;
-                        $('#country_search').dropdown();
-                    }
-                ).error(
-                function (data) {
-                    console.log(data);
-                }
-            );
-        }
-
-        function getAllDepartments() {
-            DepartmentService.getAllDepartments()
-                .success(
-                    function (data) {
-                        $scope.departments = data.departments;
-                        $('#dept_search').dropdown();
-                    }
-                ).error(
-                function (data) {
-                    console.log(data);
-                }
-            );
-        }
-
-        function getAllYears() {
-            DepartmentService.getAllYears()
-                .success(
-                    function (data) {
-                        $scope.years = data.years;
-                        $('#year_search').dropdown();
-                    }
-                ).error(
-                function (data) {
-                    console.log(data);
-                }
-            );
-        }
-
-        function getAllGenders() {
-            DepartmentService.getAllGenders()
-                .success(
-                    function (data) {
-                        $scope.genders = data.genders;
-                        $('#gender_search').dropdown();
-                    }
-                ).error(
-                function (data) {
-                    console.log(data);
-                }
-            );
-        }
+        $scope.CountryService = CountryService;
+        $scope.GenderService = GenderService;
+        $scope.YearService = YearService;
+        $scope.DepartmentService = DepartmentService;
+        $scope.profile = {};
 
         function updateProfile() {
             UserService.updateProfile(
@@ -115,8 +36,8 @@
                 $scope.profile.postalCode,
                 $scope.profile.country,
                 function (data) {
-                    if(data.code ==0){
-                        MessageBoxService.showSuccess('Succès !', 'Profile mis à jour avec succès !' );
+                    if (data.code == 0) {
+                        MessageBoxService.showSuccess('Succès !', 'Profile mis à jour avec succès !');
                     } else {
                         MessageBoxService.handleServiceError(data);
                     }
@@ -124,9 +45,11 @@
             );
         }
 
-        getAllDepartments();
-        getAllCountries();
-        getAllGenders();
-        getAllYears();
+        CountryService.getAllCountries(true);
+        GenderService.getAllGenders(true);
+        YearService.getAllYears(true);
+        DepartmentService.getAllDepartments(true);
 
     }
+
+})
