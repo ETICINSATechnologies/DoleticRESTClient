7a4217e5cc7a52908dddb488366745daaf4920dc
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-02-15 12:57:45 +0100

    update dashboard

--- app/components/dashboard/dashboard.controller.js
+++ app/components/dashboard/dashboard.controller.js
@@ -13,27 +13,39 @@
         $scope.sharedVariables = SharedVariables;
         $scope.currentUser = UserService.getCurrentUser();
         $scope.currentUser.activePosition = _.find($scope.currentUser.positions, 'active');
-        $scope.editState = false;
-        $scope.editProfile = false;
         $scope.showEditPassForm = showEditPassForm;
         $scope.showEditProfileForm = showEditProfileForm;
-        $scope.cancelPassForm = cancelPassForm();
-        $scope.cancelProfileForm = cancelProfileForm;
-
-        function cancelPassForm() {
-            $scope.editState = false;
-        }
-
-        function cancelProfileForm(){
-            $scope.editProfile = false;
-        }
 
         function showEditProfileForm() {
-            $scope.editProfile =true;
+            ModalService.showModal({
+                templateUrl: "app/components/dashboard/profile-form-modal/profile-form-modal.template.html",
+                controller: "ProfileFormController",
+                inputs:{
+                    editMode: false,
+                    profile:{}
+                }
+            }).then(function (modal) {
+                modal.element.modal('show');
+            }).catch(function (error) {
+                // error contains a detailed error message.
+                console.log(error);
+            });
         }
 
         function showEditPassForm() {
-            $scope.editState = true;
+            ModalService.showModal({
+                templateUrl: "app/components/dashboard/pass-form-modal/pass-form-modal.template.html",
+                controller: "PassFormController",
+                inputs:{
+                    editMode: false,
+                    pass :{}
+                }
+            }).then(function (modal) {
+                modal.element.modal('show');
+            }).catch(function (error) {
+                // error contains a detailed error message.
+                console.log(error);
+            });
         }
 
 
