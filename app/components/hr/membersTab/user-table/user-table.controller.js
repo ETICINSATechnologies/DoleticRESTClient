39de73aefacb0994cb6b2d28622cb96991f766b9
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-03-28 12:50:58 +0200

    Mise a jour RH (WIP)

--- app/components/hr/membersTab/user-table/user-table.controller.js
+++ app/components/hr/membersTab/user-table/user-table.controller.js
@@ -8,33 +8,19 @@
     hrUserTableController.$inject = ['$scope', '$state', 'UserService', 'SharedVariables', 'DTOptionsBuilder', 'DTColumnDefBuilder'];
 
     function hrUserTableController($scope, $state, UserService, SharedVariables, DTOptionsBuilder, DTColumnDefBuilder) {
-        $scope.users = {};
+        $scope.userService = UserService;
         $scope.dtOptions = DTOptionsBuilder
             .newOptions()
             .withPaginationType('full_numbers')
             .withDisplayLength(20);
         $scope.dtColumnDefs = [];
         $scope.goToUserDetailsTab = goToUserDetailsTab;
-
-        function getAllUserData() {
-            UserService.getAllUsers()
-                .success(
-                    function (data) {
-                        $scope.users = data.users;
-                        console.log($scope.users);
-                    }
-                ).error(
-                    function (data) {
-                        console.log(data);
-                    }
-                );
-        }
         
         function goToUserDetailsTab(user) {
             SharedVariables.rh.selectedUser = user;
             $state.go("hr.userDetailsTab");
         }
 
-        getAllUserData();
+        UserService.getAllUsers(true);
     }
 })();
