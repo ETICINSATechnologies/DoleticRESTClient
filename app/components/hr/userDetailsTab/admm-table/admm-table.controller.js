0d29125e503f3aba14cb171d02116a8f0873ae29
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-05-21 18:42:57 +0200

    Tableau administratorMemberships fonctionnel

--- app/components/hr/userDetailsTab/admm-table/admm-table.controller.js
+++ app/components/hr/userDetailsTab/admm-table/admm-table.controller.js
@@ -5,34 +5,66 @@
         .module('doleticApp')
         .controller('hrAdmmTableController', hrAdmmTableController);
 
-    hrAdmmTableController.$inject = ['$scope', '$state', 'AdministratorMembershipService', 'ConfirmModalService', 'MessageBoxService', 'ModalService'];
+    hrAdmmTableController.$inject = ['$scope', '$state', '$filter', 'AdministratorMembershipService', 'ConfirmModalService', 'MessageBoxService', 'ModalService', 'RHService'];
 
-    function hrAdmmTableController($scope, $state, AdministratorMembershipService, ConfirmModalService, MessageBoxService, ModalService) {
+    function hrAdmmTableController($scope, $state, $filter, AdministratorMembershipService, ConfirmModalService, MessageBoxService, ModalService, RHService) {
         $scope.administratorMembershipService = AdministratorMembershipService;
+        $scope.rhService = RHService;
+        $scope.editMode = false;
 
-        $scope.showAdmmForm = function (task) {
-            ModalService.showModal({
-                templateUrl: "app/components/hr/userDetailsTab/admm-form/task-form.template.html",
-                controller: "hrAdmmFormController",
-                inputs: {
-                    editMode: true,
-                    administratorMembership: angular.copy(task)
-                }
-            }).then(function (modal) {
-                modal.element.modal('show');
-            }).catch(function (error) {
-                // error contains a detailed error message.
-                console.log(error);
+        $scope.showMembershipForm = function(editMode, admm) {
+            $scope.administratorMembership = admm ? angular.copy(admm) : {};
+            if(editMode) {
+                formatAdministratorMembership();
+                $scope.editMode = editMode;
+            }
+        };
+
+        $scope.cancelMembershipForm = function() {
+            delete $scope.administratorMembership;
+            $scope.editMode = false;
+        };
+
+        $scope.addMembership = function() {
+            $scope.administratorMembership.user =  $state.params.id;
+            AdministratorMembershipService.postMembership($scope.administratorMembership).success(function(data) {
+                MessageBoxService.showSuccess(
+                    "Ajout réussi !",
+                    "L'adhésion a été ajoutée."
+                );
+                delete $scope.administratorMembership;
+            }).error(function(data) {
+                MessageBoxService.showError(
+                    "Echec de l'ajout...",
+                    "L'adhésion n'a pas pu être ajoutée."
+                );
             });
         };
 
-        $scope.deleteAdmm = function (task) {
+        $scope.updateMembership = function() {
+            $scope.administratorMembership.user =  $state.params.id;
+            AdministratorMembershipService.putMembership($scope.administratorMembership).success(function(data) {
+                MessageBoxService.showSuccess(
+                    "Modification réussie !",
+                    "L'adhésion a été modifiée."
+                );
+                delete $scope.administratorMembership;
+                $scope.editMode = false;
+            }).error(function(data) {
+                MessageBoxService.showError(
+                    "Echec de la modification...",
+                    "L'adhésion n'a pas pu être modifiée."
+                );
+            });
+        };
+
+        $scope.deleteMembership = function (task) {
             ConfirmModalService.showConfirmModal(
                 "Confirmer la suppression",
                 "Voulez-vous vraiment supprimer l'adhésion ?",
                 "remove",
                 function () {
-                    AdministratorMembershipService.deleteAdmm(task).success(function (data) {
+                    AdministratorMembershipService.deleteMembership(task).success(function (data) {
                         MessageBoxService.showSuccess(
                             "Suppression réussie !",
                             "L'adhésion a été supprimée."
@@ -46,6 +78,11 @@
                 }
             );
         };
+
+        function formatAdministratorMembership() {
+            if ($scope.administratorMembership.startDate) $scope.administratorMembership.startDate = $filter('date')($scope.administratorMembership.startDate, "dd/MM/y");
+            if ($scope.administratorMembership.endDate) $scope.administratorMembership.endDate = $filter('date')($scope.administratorMembership.endDate, "dd/MM/y");
+        }
 
     }
 })();
--- app/services/rh/administratorMembership.service.js
+++ app/services/rh/administratorMembership.service.js
@@ -10,17 +10,49 @@
     function AdministratorMembershipService($http, SERVER_CONFIG) {
         var server = SERVER_CONFIG.url;
         var urlBase = '/api/rh/administrator_membership';
-        var administratorMembershipService = {};
+        var administratorMembershipFactory = {};
 
-        administratorMembershipService.getAllAdministratorMemberships = function () {
+        administratorMembershipFactory.getAllAdministratorMemberships = function () {
             return $http.get(server + urlBase + 's');
         };
 
-        administratorMembershipService.getAdministratorMembership = function (id) {
+        administratorMembershipFactory.getAdministratorMembership = function (id) {
             return $http.get(server + urlBase + "/" + id);
         };
 
-        return administratorMembershipService;
+        // POST
+        administratorMembershipFactory.postMembership = function (administratorMembership) {
+            return $http.post(server + urlBase, administratorMembership).success(function (data) {
+                administratorMembershipFactory.currentUserMemberships = angular.equals(administratorMembershipFactory.currentUserMemberships, []) ?
+                    {} : administratorMembershipFactory.currentUserMemberships;
+                administratorMembershipFactory.currentUserMemberships[data.administrator_membership.id] = data.administrator_membership;
+            }).error(function (error) {
+                console.log(error);
+            });
+        };
+
+        // PUT
+        administratorMembershipFactory.putMembership = function (administratorMembership) {
+            console.log(administratorMembership);
+            return $http.post(server + urlBase + "/" + administratorMembership.id, administratorMembership).success(function (data) {
+                administratorMembershipFactory.currentUserMemberships = angular.equals(administratorMembershipFactory.currentUserMemberships, []) ?
+                    {} : administratorMembershipFactory.currentUserMemberships;
+                administratorMembershipFactory.currentUserMemberships[data.administrator_membership.id] = data.administrator_membership;
+            }).error(function (error) {
+                console.log(error);
+            });
+        };
+
+        // DELETE
+        administratorMembershipFactory.deleteMembership = function (adminstratorMembership) {
+            return $http.delete(server + urlBase + "/" + adminstratorMembership.id).success(function (data) {
+                delete administratorMembershipFactory.currentUserMemberships[adminstratorMembership.id];
+            }).error(function (error) {
+                console.log(error);
+            });
+        };
+
+        return administratorMembershipFactory;
     }
 
 })();
